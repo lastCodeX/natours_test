7e044075c6c4af4650e5e09b7cdc3a22e6fd223a
Andrey Yurov <lastcodestudio@gmail.com>
2021-03-07 22:18:40 +0300

    Add documents middleware

--- model/tourModel.js
+++ model/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 
 const tourSchema = new mongoose.Schema({
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema({
     unique: true,
     trim: true
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, 'Atour must have a duration']
@@ -63,6 +65,13 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function(){
   return this.duration / 7
 })
+// Document Middleware
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, {lower: true})
+  next()
+})
+
+
 
 const Tour = mongoose.model('Tour', tourSchema)
 
