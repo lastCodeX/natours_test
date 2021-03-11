c2bd0e3b15523350ddb628648fef9e0f95223235
Andrey Yurov <lastcodestudio@gmail.com>
2021-03-11 21:56:34 +0300

    Added handling of nonexistent paths

--- app.js
+++ app.js
@@ -13,5 +13,12 @@ app.use(express.json())
 app.use('/api/v1/tours', tourRoute)
 app.use('/api/v1/users', userRoute)
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  })
+})
+
 module.exports = app
 
