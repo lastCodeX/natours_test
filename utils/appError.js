5f66dcae20d3ddeeb14a7dcf97cee87730406df3
Andrey Yurov <lastcodestudio@gmail.com>
2021-03-12 11:57:06 +0300

    Refactoring error middleware

---
+++ utils/appError.js
@@ -0,0 +1,13 @@
+class appError extends Error {
+  constructor(message, statusCode) {
+    super(message)
+
+    this.statusCode = statusCode
+    this.status = `${statusCode}.startsWith('4') ? 'fail' : 'errors'`
+    this.isOperationel = true
+
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+module.exports = appError
