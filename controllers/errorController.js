f5ea7f30c25a2d39fa708f64aabf07e1b6322502
lastCodeX <lastcodestudio@gmail.com>
2021-03-14 09:21:19 +0300

    separation of error output into "development" and " production"

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,26 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500
-  err.status = err.status || 'error'
+const sendErrDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack
+  })
+}
 
+const sendErrProd = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message
   })
+}
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || 'error'
+
+  if(process.env.NODE_ENV === 'development') {
+    sendErrDev()
+  } else if(process.env.NODE_ENV === 'production') {
+    sendErrProd
+  }
 }
